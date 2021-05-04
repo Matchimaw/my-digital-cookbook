3ef6df80af8626803005d60638eb4a8bfb4bfb08
Matchima Wisuttisawad <pompam@MacBook-Pro.local>
2021-05-04 14:39:27 -0400

    delete img

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,25 @@
+class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipes_params)
+
+    if @recipe.save
+      flash[:success] = "Recipe added successfully"
+      redirect_to "/recipes/#{@recipe.id}"
+    else
+      flash.now[:error] = @recipe.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(:name, :photo_path, :prepTime, :ingredients, :instructions)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
+  resources :recipes, only: [:new, :create]
+
   get '/recipes', to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
-      resources :recipes, only: [:index] 
+      resources :recipes, only: [:index]
     end
   end
 end
